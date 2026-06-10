5995fa3ecf87b4adccc0764f8b8a967b6de3c690
Joao-Vitor-2008 <joaovitornascimentolopes4@gmail.com>
2026-06-09 22:10:54 -0400

    Alteração leve na config neovim

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,7 +7,7 @@ return {
     "rcarriga/nvim-dap-ui",
     dependencies = {
       "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio", -- 🔥 isso que faltava
+      "nvim-neotest/nvim-nio",
     },
     config = function()
       require("dapui").setup()
